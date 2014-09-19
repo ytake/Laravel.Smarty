5ab114b886c2de62d6ae98e649ce9d9a1f037c63
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2014-09-20 01:10:39 +0900

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -4,30 +4,30 @@
  * @author  yuuki.takezawa<yuuki.takezawa@comnect.jp.net>
  * @license http://opensource.org/licenses/MIT MIT
  */
-return [
-	// smarty file extension
-	'extension' => 'tpl',
-	//
-	'debugging' => false,
-	// use cache
-	'caching' => true,
-	//
-	'cache_lifetime' => 120,
-	//
-	'compile_check' => false,
-	// delimiters
-	// default "{$smarty}"
-	'left_delimiter' => '{',
-	'right_delimiter' => '}',
-	// path info
-	'template_path' => base_path() . '/app/views',
-	'cache_path' => base_path() . '/app/storage/smarty/cache',
-	'compile_path' => base_path() . '/app/storage/smarty/compile',
-	'plugins_paths' => array(
+return array(
+    // smarty file extension
+    'extension' => 'tpl',
+    //
+    'debugging' => false,
+    // use cache
+    'caching' => true,
+    //
+    'cache_lifetime' => 120,
+    //
+    'compile_check' => false,
+    // delimiters
+    // default "{$smarty}"
+    'left_delimiter' => '{',
+    'right_delimiter' => '}',
+    // path info
+    'template_path' => base_path() . '/app/views',
+    'cache_path' => base_path() . '/app/storage/smarty/cache',
+    'compile_path' => base_path() . '/app/storage/smarty/compile',
+    'plugins_paths' => array(
         base_path() . '/app/views/smarty/plugins',
-    ),
+        ),
     'config_paths' => array(
         base_path() . '/app/config/smarty',
     ),
     'force_compile' => false
-];
+);
