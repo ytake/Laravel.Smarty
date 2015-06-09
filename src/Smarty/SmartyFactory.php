110fa07bfc8f33f6de39a52e37165103f89a3bf1
yuuki takezawa <yuuki.takezawa@comnect.jp.net>
2015-06-09 22:24:02 +0900

    increment version

--- src/Smarty/SmartyFactory.php
+++ src/Smarty/SmartyFactory.php
@@ -22,7 +22,7 @@ class SmartyFactory extends Factory
     /**
      * @var string  version
      */
-    const VERSION = '2.0.0-dev';
+    const VERSION = '2.0.3';
 
     /** @var Smarty $smarty */
     protected $smarty;
